4a409a51904349d649cbf785f3dcbc5948cee090
YoussoufouOusmanou <39488510+YoussoufouOusmanou@users.noreply.github.com>
2021-08-28 13:13:36 +0200

    Create main.tf

---
+++ modules/sec_group/main.tf
@@ -0,0 +1,46 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "myec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instancetype
+  key_name      = "devops-ousmanou"
+  availability_zone = "us-east-1b"
+  tags            = var.tag_prenom
+  security_groups = ["${var.sec_group}"]
+  root_block_device {
+    delete_on_termination = true
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("./devops-ousmanou.pem")
+      host        = self.public_ip
+    }
+  }
+}
+
+output "aws_instance" {
+  value = aws_instance.myec2.id
+}
