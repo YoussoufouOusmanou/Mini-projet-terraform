0a7b92d359c9dd3eb2465201f58eb03adb7a7cac
Ousmanou YOUSSOUFOU <39488510+YoussoufouOusmanou@users.noreply.github.com>
2022-03-09 17:36:18 +0100

    Update main.tf

--- modules/sec_group/main.tf
+++ modules/sec_group/main.tf
@@ -1,46 +1,36 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+resource "aws_security_group" "security_group" {
+  name        = var.sg_name
+  description = "allo inbound outbount traffic"
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  owners = ["099720109477"] # Canonical
-}
-
-
-resource "aws_instance" "myec2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instancetype
-  key_name      = "devops-ousmanou"
-  availability_zone = "us-east-1b"
-  tags            = var.tag_prenom
-  security_groups = ["${var.sec_group}"]
-  root_block_device {
-    delete_on_termination = true
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get -y update",
-      "sudo apt-get -y install nginx",
-      "sudo service nginx start",
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("./devops-ousmanou.pem")
-      host        = self.public_ip
-    }
+  egress {
+    description = "outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "aws_instance" {
-  value = aws_instance.myec2.id
+output "sg_name" {
+  value = var.sg_name
 }
